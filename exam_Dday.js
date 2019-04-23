f9eca627497da10390f8356380f893991cc9d3e9
jk4654 <43900991+jk4654@users.noreply.github.com>
2019-04-23 22:36:39 +0900

    Add files via upload

--- exam_Dday.js
+++ exam_Dday.js
@@ -1,84 +1,84 @@
-
-// 1학기 중간고사
 var now = new Date();
+// 1학기 중간고사
+
 var then1 = new Date("april 29,2019 00:00:00");
 var gap1 = then1.getTime() - now.getTime() ;
 gap1 = Math.floor(gap1 / (1000 * 60 * 60 * 24)) ;
-document.write(  '4/29 &nbsp &nbsp 2학년 1학기 중간고사 D-' + gap1 +'<br><br>'  ) ;
+document.write(  '4/29 &nbsp &nbsp 2학년 1학기 중간고사 D-' + (gap1+1) +'<br><br>'  ) ;
 
 //현장체험학습
-var now = new Date();
+
 var then1_25 = new Date("may 2,2019 00:00:00");
 var gap1_25 = then1_25.getTime() - now.getTime() ;
 gap1_25 = Math.floor(gap1_25 / (1000 * 60 * 60 * 24)) ;
-document.write(  '5/2 &nbsp &nbsp 서울랜드 D-' + gap1_25 +'<br><br>'  ) ;
+document.write(  '5/2 &nbsp &nbsp 서울랜드 D-' + (gap1_25+1) +'<br><br>'  ) ;
 
 //소래산등반
-var now = new Date();
+
 var then1_5 = new Date("may 9,2019 00:00:00");
 var gap1_5 = then1_5.getTime() - now.getTime() ;
 gap1_5 = Math.floor(gap1_5 / (1000 * 60 * 60 * 24)) ;
-document.write(  '5/9 &nbsp &nbsp 소래산 등반 D-' + gap1_5 +'<br><br>'  ) ;
+document.write(  '5/9 &nbsp &nbsp 소래산 등반 D-' + (gap1_5+1) +'<br><br>'  ) ;
 
 //체육대회
-var now = new Date();
+
 var then1_8 = new Date("may 17,2019 00:00:00");
 var gap1_8 = then1_8.getTime() - now.getTime() ;
 gap1_8 = Math.floor(gap1_8 / (1000 * 60 * 60 * 24)) ;
-document.write(  '5/17 &nbsp &nbsp 체육대회 D-' + gap1_8 +'<br><br>'  ) ;
+document.write(  '5/17 &nbsp &nbsp 체육대회 D-' + (gap1_8+1) +'<br><br>'  ) ;
 
 //국가수준 학업성취도 평가
-var now = new Date();
+
 var then1_75 = new Date("june 13,2019 00:00:00");
 var gap1_75 = then1_75.getTime() - now.getTime() ;
 gap1_75 = Math.floor(gap1_75 / (1000 * 60 * 60 * 24)) ;
-document.write(  '6/13 &nbsp &nbsp 국가수준 학업성취도 평가 D-' + gap1_75 +'<br><br>'  ) ;
+document.write(  '6/13 &nbsp &nbsp 국가수준 학업성취도 평가 D-' + (gap1_75+1) +'<br><br>'  ) ;
 
 //1학기 기말고사
-var now = new Date();
+
 var then2 = new Date("july 3,2019 00:00:00");
 var gap2 = then2.getTime() - now.getTime() ;
 gap2 = Math.floor(gap2 / (1000 * 60 * 60 * 24)) ;
-document.write(  '7/3 &nbsp &nbsp 2학년 1학기 기말고사 D-' + gap2 +'<br><br>'  ) ;
+document.write(  '7/3 &nbsp &nbsp 2학년 1학기 기말고사 D-' + (gap2+1) +'<br><br>'  ) ;
 
 //여름방학식
-var now = new Date();
+
 var then2_25 = new Date("july 19,2019 00:00:00");
 var gap2_25 = then2_25.getTime() - now.getTime() ;
 gap2_25 = Math.floor(gap2_25 / (1000 * 60 * 60 * 24)) ;
-document.write(  '7/19 &nbsp &nbsp 여름방학식 D-' + gap2_25 +'</br>'  ) ;
+document.write( ' 7/19 &nbsp &nbsp 여름방학식 D-' + (gap2_25+1) +'</br>' ) ;
 
 //개학
 var now = new Date();
 var then2_5 = new Date("august 16,2019 00:00:00");
 var gap2_5 = then2_5.getTime() - now.getTime() ;
 gap2_5 = Math.floor(gap2_5 / (1000 * 60 * 60 * 24)) ;
-document.write(  '8/16 &nbsp &nbsp 개학 D-' + gap2_5 +'<br><br>'  ) ;
+document.write(  '8/16 &nbsp &nbsp 개학 D-' + (gap2_5+1) +'<br><br>'  ) ;
 
 //2학년 2학기 중간고사
 var now = new Date();
 var then3 = new Date("september 24,2019 00:00:00");
 var gap3 = then3.getTime() - now.getTime() ;
 gap3 = Math.floor(gap3 / (1000 * 60 * 60 * 24)) ;
-document.write(  '9/24 &nbsp &nbsp 2학년 2학기 중간고사 D-' + gap3 +'<br><br>'  ) ;
+document.write(  '9/24 &nbsp &nbsp 2학년 2학기 중간고사 D-' + (gap3+1) +'<br><br>'  ) ;
 
 //웃터골예술제
 var now = new Date();
 var then3_25 = new Date("october 25,2019 00:00:00");
 var gap3_25 = then3_25.getTime() - now.getTime() ;
 gap3_25 = Math.floor(gap3_25 / (1000 * 60 * 60 * 24)) ;
-document.write(  '10/25 &nbsp &nbsp 웃터골예술제 D-' + gap3_25 +'<br><br>'  ) ;
+document.write(  '10/25 &nbsp &nbsp 웃터골예술제 D-' + (gap3_25+1) +'<br><br>'  ) ;
 
 //2학년 2학기 기말고사
 var now = new Date();
 var then4 = new Date("december 9,2019 00:00:00");
 var gap4 = then4.getTime() - now.getTime() ;
 gap4 = Math.floor(gap4 / (1000 * 60 * 60 * 24)) ;
-document.write(  '12/9 &nbsp &nbsp 2학년 2학기 기말고사 D-' + gap4 +'<br><br>'  ) ;
+document.write(  '12/9 &nbsp &nbsp 2학년 2학기 기말고사 D-' + (gap4+1) +'<br><br>'  ) ;
 
 //종업식
 var now = new Date();
 var then5 = new Date("january 7,2020 00:00:00");
 var gap5 = then5.getTime() - now.getTime() ;
 gap5 = Math.floor(gap5 / (1000 * 60 * 60 * 24)) ;
-document.write(  '1/7 종업식 D-' + gap5 +'<br><br>'  ) ;
+document.write(  '1/7 종업식 D-' + (gap5+1) +'<br><br>'  ) ;
